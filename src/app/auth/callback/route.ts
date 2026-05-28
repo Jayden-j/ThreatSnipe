f0580da5dd36b6919410c33e55c4d689b788c193
Jayden-j <jaydenisaplaystation@gmail.com>
2026-05-28 16:52:29 -0400

    fixed callback google oauth bug

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -9,7 +9,8 @@ export async function GET(request: NextRequest) {
     return NextResponse.redirect(`${origin}/login`);
   }
 
-  let supabaseResponse = NextResponse.redirect(`${origin}/dashboard`);
+  const redirectTo = `${origin}/dashboard`;
+  const supabaseResponse = NextResponse.redirect(redirectTo);
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -20,10 +21,6 @@ export async function GET(request: NextRequest) {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          );
-          supabaseResponse = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
           );
